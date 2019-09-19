4643febb29c506b03c9be0e5786ae5ab29cb64cd
Eric Liu <eric.liu@pwc.com>
2019-09-19 15:52:30 +0800

    002

--- src/Content.js
+++ src/Content.js
@@ -3,10 +3,31 @@ import PropTypes from 'prop-types'
 import ThemeSwitch from './ThemeSwitch'
 
 class Content extends Component {
+  static contextTypes = {
+    store: PropTypes.object
+  }
+
+  constructor () {
+    super()
+    this.state = { themeColor: '' }
+  }
+
+  componentWillMount () {
+    const { store } = this.context
+    this._updateThemeColor()
+    store.subscribe(() => this._updateThemeColor())
+  }
+
+  _updateThemeColor () {
+    const { store } = this.context
+    const state = store.getState()
+    this.setState({ themeColor: state.themeColor })
+  }
+
   render () {
     return (
       <div>
-        <p>This is Content</p>
+        <p style={{ color: this.state.themeColor }}>This is Content</p>
         <ThemeSwitch />
       </div>
     )
